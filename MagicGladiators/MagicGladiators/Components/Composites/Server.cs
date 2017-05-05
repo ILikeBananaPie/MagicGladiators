3210af3be01c2eac0f3aa5991b0362a614912801
ILikeBananaPie <fishyniden@gmail.com>
2017-05-05 12:54:28 +0200

    Temp

---
+++ MagicGladiators/MagicGladiators/Components/Composites/Server.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Diagnostics;
+
+namespace MagicGladiators.Components.Composites
+{
+    class Server: Component, IDrawable, ILoadable
+    {
+        private Dictionary<Connection, GameObject> players;
+
+
+        public Server(GameObject gameObject) : base(gameObject)
+        {
+            players = new Dictionary<Connection, GameObject>();
+
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("JoinServer", JoinServer);
+
+            NetworkComms.AppendGlobalIncomingPacketHandler<Vector2>("UpdatePosition", UpdatePosition);
+            //Start listening for incoming connections
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0));
+            
+        }
+
+        SpriteFont spriteFont;
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int incriment = 0;
+            foreach (System.Net.IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+            {
+                spriteBatch.DrawString(spriteFont, localEndPoint.Address.ToString() + ":" + localEndPoint.Port.ToString(), new Vector2(20, 20 + incriment), Color.Black);
+                incriment += 15;
+            }
+        }
+
+        private void JoinServer(PacketHeader header, Connection connection, string message)
+        {
+            if (players.Count <= 8)
+            {
+                if (!players.ContainsKey(connection))
+                {
+                    GameObject go = new GameObject(0);
+                    connection.SendObject<bool>("JoinedServerRespond", true);
+                } else
+                {
+                    connection.SendObject<bool>("JoinedServerRespond", false);
+                }
+            } else
+            {
+                connection.SendObject<bool>("JoinedServerRespond", false);
+            }
+        }
+
+        private void UpdatePosition(PacketHeader header, Connection connection, Vector2 position)
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            spriteFont = content.Load<SpriteFont>("Font");
+        }
+    }
+}
