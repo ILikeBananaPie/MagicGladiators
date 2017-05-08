660e4bc51eb0ee6de25c13d590889e867105ed3a
Nitro <Nitro@DESKTOP-OEKLE1E>
2017-05-08 16:26:01 +0200

    comments

--- MagicGladiators/MagicGladiators/GameWorld.cs
+++ MagicGladiators/MagicGladiators/GameWorld.cs
@@ -64,7 +64,7 @@ namespace MagicGladiators
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
         /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
+        /// and initialize them as well...
         /// </summary>
         protected override void Initialize()
         {
