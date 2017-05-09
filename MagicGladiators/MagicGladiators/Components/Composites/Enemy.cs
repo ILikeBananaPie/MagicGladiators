989528def33a7df832301b59b9f91e59fd5b064d
ILikeBananaPie <fishyniden@gmail.com>
2017-05-09 12:45:06 +0200

    Updated enemy UpdatePackage Info Transform got a threadlock

---
+++ MagicGladiators/MagicGladiators/Components/Composites/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace MagicGladiators.Components.Composites
+{
+    class Enemy:Component, ILoadable, IUpdateable
+    {
+        private Transform trnsfrm;
+        private Vector2 direction;
+
+        public Enemy/*Number 1*/(GameObject gameObject) : base(gameObject)
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            trnsfrm = gameObject.GetComponent("Transform") as Transform;
+        }
+
+        public void Update()
+        {
+            trnsfrm.Translate(direction * GameWorld.Instance.deltaTime);
+        }
+
+        public void UpdateEnemyInfo(UpdatePackage package)
+        {
+            this.trnsfrm.position = package.position;
+            this.direction = package.direction;
+        }
+    }
+}
--- MagicGladiators/MagicGladiators/UpdatePackage.cs
+++ MagicGladiators/MagicGladiators/UpdatePackage.cs
@@ -12,15 +12,34 @@ namespace MagicGladiators
     public class UpdatePackage
     {
         public Vector2 position { get; set; }
+        public Vector2 direction { get; set; }
 
         [ProtoMember(1)]
-        private float[] xy;
+        private float[] xypos;
+
+        [ProtoMember(2)]
+        private float[] xydir;
 
         private UpdatePackage() { }
 
+        /// <summary>
+        /// Update package to communicate with server
+        /// </summary>
+        /// <param name="position">Your characters current position</param>
         public UpdatePackage(Vector2 position)
         {
             this.position = position;
+            this.direction = Vector2.Zero;
+        }
+        /// <summary>
+        /// Update package to communicate with server
+        /// </summary>
+        /// <param name="position">Your characters current position</param>
+        /// <param name="direction">Your characters current direction. Should be normalized.</param>
+        public UpdatePackage(Vector2 position, Vector2 direction)
+        {
+            this.position = position;
+            this.direction = direction;
         }
 
         [ProtoBeforeSerialization]
@@ -28,14 +47,19 @@ namespace MagicGladiators
         {
             if (position != null)
             {
-                xy = new float[2] { position.X, position.Y };
+                xypos = new float[2] { position.X, position.Y };
+            }
+            if (position != null)
+            {
+                xydir = new float[2] { direction.X, direction.Y };
             }
         }
 
         [ProtoAfterDeserialization]
         private void Deserialize()
         {
-            position = new Vector2(xy[0], xy[1]);
+            position = new Vector2(xypos[0], xypos[1]);
+            direction = new Vector2(xydir[0], xydir[1]);
         }
     }
 }
