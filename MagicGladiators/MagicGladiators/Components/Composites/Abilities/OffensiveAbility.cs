a9e51d721e53b92c39051b62a974d6b5f37c1056
ILikeBananaPie <fishyniden@gmail.com>
2017-05-12 12:55:56 +0200

    I guess

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/OffensiveAbility.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/OffensiveAbility.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework.Input;
 
 namespace MagicGladiators.Components.Composites.Abilities
 {
     abstract class OffensiveAbility:Component, ILoadable, IUpdateable
     {
         //Something akin to missiles mainly
-        public OffensiveAbility(GameObject go) : base(go)
+        public OffensiveAbility(GameObject go, MouseState ms) : base(go)
         {
         }
 
