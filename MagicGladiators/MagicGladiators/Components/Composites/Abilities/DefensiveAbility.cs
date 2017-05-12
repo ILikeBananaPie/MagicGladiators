b9e6becac5809253fc2b23c3aba4c0e7f9d10004
Steffen Schjønning Jørgensen <steffentidus@hotmail.com>
2017-05-12 14:20:16 +0200

    tgg

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/DefensiveAbility.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/DefensiveAbility.cs
@@ -9,9 +9,7 @@ namespace MagicGladiators.Components.Composites.Abilities
 {
     abstract class DefensiveAbility:Component, ILoadable, IUpdateable
     {
-        /// <summary>
-        /// ldsjflksjd
-        /// </summary>
+        
         /// <param name="go"></param>
         //Something stationary or character based
         public DefensiveAbility(GameObject go) : base(go)
