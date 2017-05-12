87fca8cb1c68c20f740c01aca4eee852dac267c7
Steffen Schjønning Jørgensen <steffentidus@hotmail.com>
2017-05-12 18:42:25 +0200

    to be continued

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/Mobility/Charge.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/Mobility/Charge.cs
@@ -8,8 +8,75 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
 namespace MagicGladiators
 {
-    class Charge
+    class Charge : MobilityAbility, IUpdateable
     {
+
+        private bool on = false;
+
+
+        private Animator animator; //test 
+
+        private IStrategy strategy;
+
+        private DIRECTION direction;
+
+        private GameObject go;
+        private Transform transform;
+        private Vector2 originalPos;
+        private Vector2 testVector;
+
+        private Vector2 target;
+
+
+        private Physics physics;
+        public Charge(GameObject go, Transform transform, Animator animator) : base(go)
+        {
+
+            this.animator = animator;
+            this.transform = transform;
+            this.physics = (transform.gameObject.GetComponent("Physics") as Physics);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            MouseState mouse = Mouse.GetState();
+            KeyboardState keyState = Keyboard.GetState();
+
+
+
+            Vector2 translation = originalPos;
+
+
+            //activate aim and move in a charge
+
+            if (keyState.IsKeyDown(Keys.Space) && !on)
+            {
+                on = true;
+            }
+            else if ( keyState.IsKeyDown(Keys.Space) && on)
+            {
+                on = false;
+            }
+            if(on && mouse.LeftButton == ButtonState.Pressed)
+            {
+                physics.Acceleration += new Vector2(1.25F, 0);
+            }
+               // physics.Acceleration += new Vector2(1.25F, 0);
+                /*if ()
+                {
+
+                }
+                */
+            }
+
+            //animator.PlayAnimation("ChargeRight");
+
+        }
 
     }
 
@@ -17,7 +84,7 @@ namespace MagicGladiators
 
 
 
-}
+
 
 
     
