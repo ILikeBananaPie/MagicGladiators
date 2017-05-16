c1fce33d7fff6d62109491bcc62485269e87f152
Nitro <Nitro@DESKTOP-OEKLE1E>
2017-05-16 23:15:30 +0200

    Deflect done

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/Defensive/Deflect.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/Defensive/Deflect.cs
@@ -11,7 +11,7 @@ namespace MagicGladiators
 {
     class Deflect : DefensiveAbility
     {
-        private float cooldown = 5;
+        private float cooldown = 1;
         private bool canUse = true;
         private float timer;
         private bool activated = false;
@@ -61,11 +61,59 @@ namespace MagicGladiators
                     {
                         Circle playerCircle = new Circle();
                         playerCircle.Center = gameObject.transform.position;
-                        playerCircle.Radius = (gameObject.GetComponent("Collider") as Collider).CircleCollisionBox.Radius * 2;
+                        playerCircle.Radius = (gameObject.GetComponent("Collider") as Collider).CircleCollisionBox.Radius * 1.2F;
 
                         if (playerCircle.Intersects((go.GetComponent("Collider") as Collider).CircleCollisionBox))
                         {
-                            (go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * -1;
+                            Vector2 player = new Vector2(gameObject.transform.position.X + 16, gameObject.transform.position.Y + 16);
+                            Vector2 other = new Vector2(go.transform.position.X + 16, go.transform.position.Y + 16);
+                            activated = false;
+                            if (player.X - other.X > player.Y - other.Y && other.X < player.X && other.Y > player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+                            }
+                            if (player.X - other.X < player.Y - other.Y && other.X < player.X && other.Y > player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X, (go.GetComponent("Physics") as Physics).Velocity.Y * -1);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X > player.Y - other.Y && other.X > player.X && other.Y < player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X, (go.GetComponent("Physics") as Physics).Velocity.Y * -1);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X < player.Y - other.Y && other.X > player.X && other.Y < player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X > player.Y - other.Y && other.X < player.X && other.Y < player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X, (go.GetComponent("Physics") as Physics).Velocity.Y * -1);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X < player.Y - other.Y && other.X < player.X && other.Y < player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X > player.Y - other.Y && other.X > player.X && other.Y > player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X, (go.GetComponent("Physics") as Physics).Velocity.Y * -1);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+
+                            }
+                            if (player.X - other.X < player.Y - other.Y && other.X > player.X && other.Y > player.Y)
+                            {
+                                (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
+                                //(go.GetComponent("Physics") as Physics).Velocity = (go.GetComponent("Physics") as Physics).Velocity * 1.1F;
+                            }
                         }
                     }
                 }
