851e24a6b694b8e786fe989bd99cc6ae35d72580
Nitro <Nitro@DESKTOP-OEKLE1E>
2017-05-19 20:50:22 +0200

    Updated deflect

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/Defensive/Deflect.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/Defensive/Deflect.cs
@@ -106,6 +106,53 @@ namespace MagicGladiators
                             Vector2 other = new Vector2(go.transform.position.X + 16, go.transform.position.Y + 16);
                             activated = false;
 
+                            Physics physicsSpell = (go.GetComponent("Physics") as Physics);
+                            Physics physicsPlayer = (gameObject.GetComponent("Physics") as Physics);
+
+                            //float newVelX = (physicsSpell.Velocity.X * (1 - 1) + (2 * 1 * physicsPlayer.Velocity.X)) / (1 + 1);
+                            //float newVelY = (physicsSpell.Velocity.Y * (1 - 1) + (2 * 1 * physicsPlayer.Velocity.Y)) / (1 + 1);
+
+                            float L = Vector2.Distance(gameObject.transform.position, go.transform.position);
+                            float ex = (gameObject.transform.position.X - go.transform.position.X) / L;
+                            float ey = (gameObject.transform.position.Y - go.transform.position.Y) / L;
+
+                            float ox = -1 * ey;
+                            float oy = ex;
+
+
+                            double e1x = (physicsPlayer.Velocity.X * ex + physicsPlayer.Velocity.Y * ey) * ex;
+                            double e1y = (physicsPlayer.Velocity.X * ex + physicsPlayer.Velocity.Y * ey) * ey;
+                            double e2x = (physicsSpell.Velocity.X * ex + physicsSpell.Velocity.Y * ey) * ex;
+                            double e2y = (physicsSpell.Velocity.X * ex + physicsSpell.Velocity.Y * ey) * ey;
+
+                            double o1x = (physicsPlayer.Velocity.X * ox + physicsPlayer.Velocity.Y * oy) * ox;
+                            double o1y = (physicsPlayer.Velocity.X * ox + physicsPlayer.Velocity.Y * oy) * oy;
+                            double o2x = (physicsSpell.Velocity.X * ox + physicsSpell.Velocity.Y * oy) * ox;
+                            double o2y = (physicsSpell.Velocity.X * ox + physicsSpell.Velocity.Y * oy) * oy;
+
+                            int playerMass = 10;
+                            int spellMass = 1;
+                            double vxs = (playerMass * e1x + spellMass * e2x) / (playerMass + spellMass);
+                            double vys = (playerMass * e1y + spellMass * e2y) / (playerMass + spellMass);
+
+                            //Velocity Ball 1 after Collision
+                            double vx1 = -e1x + 2 * vxs + o1x;
+                            double vy1 = -e1y + 2 * vys + o1y;
+                            //Velocity Ball 2 after Collision
+                            double vx2 = -e2x + 2 * vxs + o2x;
+                            double vy2 = -e2y + 2 * vys + o2y;
+
+                            //ball1.Velocity.X = (float)vx1;
+                            //ball1.Velocity.Y = (float)vy1;
+
+                            //ball2.Velocity.X = (float)vx2;
+                            //ball2.Velocity.Y = (float)vy2;
+
+
+                            (go.GetComponent("Physics") as Physics).Velocity = new Vector2((float)vx2, (float)vy2);
+                            //(gameObject.GetComponent("Physics") as Physics).Velocity = new Vector2((float)vx1, (float)vy1);
+                            //go.transform.position = new Vector2(go.transform.position.X + newVelX, go.transform.position.Y + newVelY);
+                            /*
                             if (player.X - other.X > player.Y - other.Y && other.X < player.X && other.Y > player.Y)
                             {
                                 (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
@@ -146,6 +193,8 @@ namespace MagicGladiators
                                 (go.GetComponent("Physics") as Physics).Velocity = new Vector2((go.GetComponent("Physics") as Physics).Velocity.X * -1, (go.GetComponent("Physics") as Physics).Velocity.Y);
                                 (go.GetComponent("Projectile") as Projectile).TestVector = new Vector2((go.GetComponent("Projectile") as Projectile).TestVector.X * -1, (go.GetComponent("Projectile") as Projectile).TestVector.Y);
                             }
+                            */
+
                         }
                     }
                 }
