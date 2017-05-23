cafd17a758f9a6987cb545e4c75c3063b8e2b433
Nitro <Nitro@DESKTOP-OEKLE1E>
2017-05-23 12:30:40 +0200

    update

--- MagicGladiators/MagicGladiators/Components/Composites/Server.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Server.cs
@@ -43,7 +43,7 @@ namespace MagicGladiators.Components.Composites
             {
                 if (localEndPoint.Address.ToString().Contains("127."))
                 {
-                    oneself = UDPConnection.GetConnection(new ConnectionInfo(localEndPoint), UDPOptions.None, false, false);
+                    oneself = UDPConnection.GetConnection(new ConnectionInfo(localEndPoint), UDPOptions.Handshake, true, true);
                 }
             }
             Addself();
