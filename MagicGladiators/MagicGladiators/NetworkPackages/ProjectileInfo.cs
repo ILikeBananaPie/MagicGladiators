2c298c9dc9e3f7e9bc1869c7666723ada8308105
ILikeBananaPie <fishyniden@gmail.com>
2017-05-23 16:19:15 +0200

    YAY

---
+++ MagicGladiators/MagicGladiators/NetworkPackages/ProjectileInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicGladiators
+{
+    class ProjectileInfo
+    {
+        public string ProjectileName { get; private set; }
+        public Vector2 Position { get; private set; }
+        public Vector2 Velocity { get; private set; }
+        public ProjectileInfo(string name, Vector2 position, Vector2 velocity)
+        {
+            ProjectileName = name;
+            Position = position;
+            Velocity = velocity;
+        }
+    }
+}
