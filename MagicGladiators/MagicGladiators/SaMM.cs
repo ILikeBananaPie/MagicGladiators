a13eaf94eec90e89f0d156d7b0ca512b471d991a
Nitro <Nitro@DESKTOP-OEKLE1E>
2017-06-06 14:29:26 +0200

    music update

--- MagicGladiators/MagicGladiators/SaMM.cs
+++ MagicGladiators/MagicGladiators/SaMM.cs
@@ -47,66 +47,37 @@ namespace MagicGladiators
 
         private SaMM()
         {
-            volume = 100;
+            volume = 0.06F;
             volumeradiant = 0;
             changingSong = false;
         }
 
         public void Update()
         {
-            //if (changingSong)
-            //{
-            //    if (volumeradiant > 0)
-            //    {
-            //        volumeradiant -= GameWorld.Instance.deltaTime;
-            //    }
-            //    if (volumeradiant <= 0)
-            //    {
-            //        changingSong = false;
-            //        volumeradiant = 0;
-            //        if (newSong == SongState.Menu)
-            //        {
-            //            MediaPlayer.Play(mbgm);
-            //        }
-            //        if (newSong == SongState.Battle)
-            //        {
-            //            MediaPlayer.Play(bbgm);
-            //        }
-            //        lastSong = newSong;
-            //    }
-            //} else
-            //{
-            //    if (volumeradiant < 100)
-            //    {
-            //        volumeradiant += GameWorld.Instance.deltaTime;
-            //    }
-            //    if (volumeradiant > 100)
-            //    {
-            //        volumeradiant = 100;
-            //    }
-            //}
-
-            //if (lastSong != newSong)
-            //{
-            //    changingSong = true;
-            //}
-
-            //MediaPlayer.Volume = (volume / 100) * volumeradiant;
+            //MediaPlayer.Volume = (volume / 100);
         }
 
         public void LoadContent(ContentManager content)
         {
-            /*
+            
             mbgm = content.Load<Song>("All This");
             bbgm = content.Load<Song>("Alchemists Tower");
             MediaPlayer.Play(mbgm);
             MediaPlayer.IsRepeating = true;
-            */
+            
         }
 
         public void NewPlaystate(SongState ss)
         {
             newSong = ss;
+            if (ss == SongState.Battle)
+            {
+                MediaPlayer.Play(bbgm);
+            }
+            if (ss == SongState.Menu)
+            {
+                MediaPlayer.Play(mbgm);
+            }
         }
     }
 }
