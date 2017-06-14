8fcd8d70c695623bd6739dc678725741e145282f
Steffen Schjønning <steffentidus@hotmail.com>
2017-06-14 11:58:13 +0200

    .

--- MagicGladiators/MagicGladiators/Components/Composites/Abilities/DeathAbilities/DeathMine.cs
+++ MagicGladiators/MagicGladiators/Components/Composites/Abilities/DeathAbilities/DeathMine.cs
@@ -27,8 +27,7 @@ namespace MagicGladiators
         public DeathMine(GameObject gameObject, Vector2 position) : base(gameObject)
         {
            
-            this.transform = transform;
-            this.animator = animator;
+           
             Name = "DeathMine";
             cooldown = 20;
         }
@@ -40,26 +39,7 @@ namespace MagicGladiators
 
         }
 
-        private void CreateAnimations()
-        {
-            SpriteRenderer spriteRenderer = (SpriteRenderer)gameObject.GetComponent("SpriteRenderer");
-
-
-
-            animator.CreateAnimation("IdleFront", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("IdleBack", new Animation(1, 0, 0, 32, 32, 10, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("IdleLeft", new Animation(1, 0, 0, 32, 32, 10, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("IdleRight", new Animation(1, 0, 0, 32, 32, 10, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("WalkFront", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("WalkBack", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("WalkLeft", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("WalkRight", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-            animator.CreateAnimation("Shoot", new Animation(1, 0, 0, 32, 32, 6, Vector2.Zero, spriteRenderer.Sprite));
-
-            animator.PlayAnimation("Shoot");
-
-            strategy = new Idle(animator);
-        }
+      
 
        
 
